abc00ec738058a64ec6279f3317567af39813ef6
Ziphil <yfe23457@nifty.com>
2019-09-29 12:55:57 +0900

    ページ番号を仮デザインで追加

--- template/common.rb
+++ template/common.rb
@@ -3,5 +3,25 @@
 
 converter.set("page-number") do |element, position|
   this = Nodes[]
+  this << Element.build("fo:block-container") do |this|
+    this["height"] = "#{FOOTER_EXTENT} + #{BLEED_SIZE}"
+    this["margin-bottom"] = "-1 * #{BLEED_SIZE}"
+    this["margin-left"] = "-1 * #{BLEED_SIZE}"
+    this["margin-right"] = "-1 * #{BLEED_SIZE}"
+    this << Element.build("fo:block-container") do |this|
+      this["bottom"] = "8mm"
+      this["#{position}"] = "8mm"
+      this["width"] = "10mm"
+      this["font-family"] = SPECIAL_FONT_FAMILY
+      this["font-size"] = "1.2em"
+      this["text-align"] = "#{position}"
+      this["display-align"] = "after"
+      this["absolute-position"] = "absolute"
+      this << Element.build("fo:block") do |this|
+        this.fix_text_position
+        this << Element.new("fo:page-number")
+      end
+    end
+  end
   next this
 end
